0c61b86937780d555d389ff394ba91d8152ab478
Diego Rueda <diego.rueda2@gmail.com>
2017-06-01 16:25:16 -0400

    add method for singleton

--- Singleton.php
+++ Singleton.php
@@ -6,7 +6,52 @@
  * Date: 6/1/2017
  * Time: 3:56 PM
  */
-class Singleton
+class SingletonDBConnection
 {
+    //contenedor de la instancia del singleton
+    static private $instances;
 
+    //un constructor privado ecita la creacion d eun nuevo objeto
+    private function __construct()
+    {
+
+    }
+
+    //metodo del singleton que debe ser static
+    public static function getInstance()
+    {
+        if(!isset(self::$instance)){
+            $myClass = __CLASS__;
+            self::$instances = new $myClass;
+        }
+
+        return self::$instances;
+    }
+
+    public function query($data)
+    {
+        return null;
+    }
+
+    public function connectDB($data)
+    {
+        return null;
+    }
+
+    public function __wakeup(){
+        throw new Exception('Class  '.__CLASS__ . 'cannot be unserialized');
+    }
+
+    public function __sleep(){
+        throw new Exception('Class  '.__CLASS__ . 'cannot be serialized');
+    }
+
+    //evita que el objeto se pueda clonar
+    public function __clone(){
+        throw new Exception('Class  '.__CLASS__ . 'cannot be cloned');
+    }
 }
+
+//example use
+$test = SingletonDBConnection::getInstance();
+$tes->connectDB($data);
