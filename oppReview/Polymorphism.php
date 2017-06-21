dbae67395b68f760d3d670bb8a5352a11f44e895
Diego Rueda <diego.rueda2@gmail.com>
2017-06-21 16:44:19 -0400

    Polymorphism sample

---
+++ oppReview/Polymorphism.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ideasoft
+ * Date: 6/21/2017
+ * Time: 4:17 PM
+ */
+
+namespace Polymorphismo;
+
+
+interface Shape{
+    public function clacArea();
+
+}
+
+class Circle implements Shape
+{
+    private $radius;
+
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function clacArea()
+    {
+        return $this->radius*$this->radius*pi();
+    }
+}
+
+class Rectangle implements Shape
+{
+    private $width;
+    private $heught;
+
+    public function __construct($w, $h)
+    {
+        $this->width = $w;
+        $this->heught = $h;
+    }
+
+    public function clacArea()
+    {
+        return $this->width * $this->heught;
+    }
+}
+
+
+$circ = new Circle(5);
+$rect = new Rectangle(6,8);
+
+echo $circ->clacArea();
+echo $rect->clacArea();
